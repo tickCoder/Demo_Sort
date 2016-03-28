9a8fe8b956e794acb3ad8092b48b12c6c12bc621
tickCoder <tickCoder@users.noreply.github.com>
2016-03-28 09:50:45 +0800

    add project

---
+++ Demo_Sort/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Demo_Sort
+//
+//  Created by Claris on 2016.03.28.Monday.
+//  Copyright © 2016 tickCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Demo_Sort/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Demo_Sort
+//
+//  Created by Claris on 2016.03.28.Monday.
+//  Copyright © 2016 tickCoder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
